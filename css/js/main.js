3980536ae0fae0eee147764adf0f11d3255be7c7
HASSOUNI-NEY <hassanahmedomar10@gmail.com>
2026-07-05 20:34:15 +0300

    feat: création du Hero avec image de fond et compte à rebours

--- css/js/main.js
+++ css/js/main.js
@@ -27,3 +27,31 @@ if (toggle) {
     document.body.classList.toggle("dark");
   });
 }
+/* COMPTE A REBOURS */
+
+const eventDate = new Date("November 15, 2026 09:00:00").getTime();
+
+setInterval(function(){
+
+    const now = new Date().getTime();
+
+    const distance = eventDate - now;
+
+    const days = Math.floor(distance/(1000*60*60*24));
+
+    const hours = Math.floor((distance%(1000*60*60*24))/(1000*60*60));
+
+    const minutes = Math.floor((distance%(1000*60*60))/(1000*60));
+
+    const seconds = Math.floor((distance%(1000*60))/1000);
+
+    if(document.getElementById("days")){
+
+        document.getElementById("days").innerHTML=days;
+        document.getElementById("hours").innerHTML=hours;
+        document.getElementById("minutes").innerHTML=minutes;
+        document.getElementById("seconds").innerHTML=seconds;
+
+    }
+
+},1000);
